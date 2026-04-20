6d0b3280fd0ae4a9483fbd1812428d4005d21b1c
RITURAJ-ARENA <majrituraj@gmail.com>
2026-04-21 02:10:12 +0530

    Done Test-01

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -24,4 +24,30 @@
  */
 export function getTicketPrice(age, isWeekend) {
   // Your code here
+
+  let price;
+
+  if (age<0){
+    return -1;
+  }
+
+  if (age<=12){
+    price=8;
+  }
+  else if (age<18){
+    price=12;
+  }
+  else if (age<59){
+    price=15;
+  }
+  else{
+    price=10;
+  }
+
+  if (isWeekend) {
+    price+=3;
+  }
+
+  return price;
+
 }
